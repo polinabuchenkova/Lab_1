8658c3a79ddf1db17c934e74309efd1e31e0798d
Buchenkova Polina <buchenkova.polina@bk.ru>
2021-09-17 19:46:26 +0300

    Лабораторная работа №1

---
+++ 1.3.py
@@ -0,0 +1,4 @@
+phrase1 = str(input("Введите первую фразу:"))
+phrase2 = str(input("Введите вторую фразу:"))
+phrase3 = str(input("Введите третью фразу:"))
+print("\n", phrase3, "\n", phrase2, "\n", phrase1)
